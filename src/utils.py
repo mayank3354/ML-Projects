024c7cf52ff5085d0e63fce528ba3dec9e4e37ba
mayank3354 <mayankkhs@gmail.com>
2023-04-18 18:18:24 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -14,7 +14,7 @@ def save_object(file_path, obj):
 
         os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path, "wb") as file_obj:
+        with open(file_path, "wb") as file_obj: 
             dill.dump(obj, file_obj)
 
     except Exception as e:
@@ -48,3 +48,9 @@ def evaluate_model(x_train,y_train,x_test,y_test,models,param):
 
     except Exception as  e :
         raise CustomException(e,sys)              
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)    
